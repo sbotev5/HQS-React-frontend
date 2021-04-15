db8e2fa3aa34e4eaed09866056ade8d5b974be3b
sbotev5 <sbotev53@gmail.com>
2021-04-15 21:07:04 +0100

    Startup files and view all tickets functionality

---
+++ HQS-frontend/hqs-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,26 @@
+  
+import React, { Component } from 'react'
+
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand bg-dark">
+                    <div><a className="navbar-name">Help Queue Management System</a></div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent
